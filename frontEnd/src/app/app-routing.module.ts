fd900786f7a211dbd86e13e2d4cb688186fb4d4d
ekukovec <73325770+ekukovec@users.noreply.github.com>
2020-10-24 08:48:14 +0200

    front end
    new front end

--- frontEnd/src/app/app-routing.module.ts
+++ frontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SloveniaComponent } from './_components/slovenia/slovenia.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: SloveniaComponent,
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SloveniaComponent } from './_components/slovenia/slovenia.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SloveniaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
