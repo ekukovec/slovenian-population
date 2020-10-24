d5172af30c3f72063aeac85578e07b43bdc06a49
ekukovec <73325770+ekukovec@users.noreply.github.com>
2020-10-24 11:41:55 +0200

    region view
    Added regions in slovenia with Toolkip.

---
+++ frontEnd/src/app/_components/slovenia/slo-region/slo-region.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slo-region',
+  templateUrl: './slo-region.component.html',
+  styleUrls: ['./slo-region.component.scss']
+})
+export class SloRegionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- frontEnd/src/app/_components/slovenia/slovenia.component.ts
+++ frontEnd/src/app/_components/slovenia/slovenia.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { City } from 'src/app/_models/city';
+import { Region } from 'src/app/_models/region';
+import { State } from 'src/app/_models/state';
+import { GetDataService } from 'src/app/_services/get-data.service';
 
 @Component({
   selector: 'app-slovenia',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SloveniaComponent implements OnInit {
 
-  constructor() { }
+  states: State[];
+  regions: Region[];
+  citys: City[];
+
+  selectedValue: string;
+
+  constructor(private dataService: GetDataService) { }
 
   ngOnInit(): void {
+    this.getState();
+    this.getRegion();
+    this.getCity();
+  }
+
+  getState() {
+    this.dataService.getState().then(states => (this.states = states));
+  }
+
+  getRegion() {
+    this.dataService.getRegion().then(regions => (this.regions = regions));
+  }
+
+  getCity() {
+    this.dataService.getCity().then(citys => (this.citys = citys));
   }
 
 }
